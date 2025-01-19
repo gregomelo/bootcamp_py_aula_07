d9d389923eb6c1dccf0f15c2760949074c8ac7ca
Greg Oliveira <gregoryomelo@gmail.com>
2025-01-19 12:47:38 -0300

    fix(exercicio_01): alterada logica de validação da entrada
    Caso na lista de entrada estivesse inválida, ocorria a parada do código. Foi alterada a forma de validação de forma com que o código não pare.

--- exercicio_01.py
+++ exercicio_01.py
@@ -1,21 +1,57 @@
 """Solução do exercício 01."""
 
-from typing import List
+from typing import List, Optional
 
-from pydantic import validate_call
+from pydantic import ValidationError, validate_call
 
 
-@validate_call
-def calcular_media(valores: List[float]) -> float:
-    """Calcula a média de uma lista de valores."""
-    return sum(valores) / len(valores)
+def calcular_media(valores: List[float]) -> Optional[float]:
+    """
+    Calcula a média aritmética de uma lista de números.
+
+    Parameters
+    ----------
+    valores : List[float]
+        Lista contendo os valores numéricos para o cálculo da média.
+
+    Returns
+    -------
+    float or None
+        Retorna a média dos valores da lista se os dados forem válidos.
+        Caso contrário, retorna None e exibe uma mensagem de erro.
+
+    Notes
+    -----
+    A função `executar` está encapsulada dentro de `calcular_media` e decorada com
+    `@validate_call` do Pydantic. Isso é feito para evitar que a função decorada seja
+    acessada diretamente pelo usuário, mantendo a interface de uso mais simples e
+    controlada. Além disso, essa abordagem garante que a validação ocorra sempre que a
+    função principal for chamada, sem expor um método separado.
+
+    Examples
+    --------
+    >>> calcular_media([1, 2, 3, 4, 5])
+    3.0
+
+    >>> calcular_media(["a", 2, 3, 4, 5])
+    Erro: valores deve ser uma lista de números.
+    None
+    """
+
+    @validate_call
+    def executar(valores: List[float]) -> float:
+        return sum(valores) / len(valores)
+
+    try:
+        return executar(valores)
+    except ValidationError:
+        print("Erro: valores deve ser uma lista de números.")
+        return None
 
 
 if __name__ == "__main__":
     valores = [1, 2, 3, 4, 5]
-    media = calcular_media(valores)
-    print(media)
+    print(calcular_media(valores))
 
     valores = ["a", 2, 3, 4, 5]
-    media = calcular_media(valores)
-    print(media)
+    print(calcular_media(valores))
